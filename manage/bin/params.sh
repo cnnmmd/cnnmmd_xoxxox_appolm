3c372f36e9cdf4cfd86a797f66b2bc1e14a4f428
azsgit <10238943+azsgit@users.noreply.github.com>
2025-09-22 19:51:49 +0900

    add initial files

---
+++ manage/bin/params.sh
@@ -0,0 +1,4 @@
+pthdoc="${pthtop}"/export/cnf/xoxxox/docenv_envolm
+cnfimg="${pthtop}"/export/cnf/xoxxox/docenv_envolm.txt
+cnfcmp="${pthtop}"/export/cnf/xoxxox/docker_appolm.yml
+imgtgt='xoxxox/envolm:001'
